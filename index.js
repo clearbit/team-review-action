be7bc0c4160a0618c83db16cc0f95f4b1acf4e08
João Moura <joaomdmoura@gmail.com>
2021-01-06 18:00:17 -0300

    accounting for the creator being part of the team

--- index.js
+++ index.js
@@ -6,19 +6,25 @@ async function run() {
     let requiredReviews = [];
     const team = JSON.parse(core.getInput('team'));
     const githubToken = core.getInput('GITHUB_TOKEN');
-    const octokit = github.getOctokit(githubToken)
+    const octokit = github.getOctokit(githubToken);
+    const creator = (await octokit.pulls.get({
+      owner: github.context.payload.pull_request.base.repo.owner.login,
+      repo: github.context.payload.pull_request.base.repo.name,
+      pull_number: github.context.payload.pull_request.number,
+    })).user.login;
+
     const reviews = (await octokit.pulls.listReviews({
       owner: github.context.payload.pull_request.base.repo.owner.login,
       repo: github.context.payload.pull_request.base.repo.name,
       pull_number: github.context.payload.pull_request.number,
     })).data.map((review) => review.user.login);
-    
+
 
     team.forEach(member => {
       requiredReviews.push(reviews.includes(member));
     });
 
-    if(!requiredReviews.includes(true)) {
+    if(!team.includes(creator) && !requiredReviews.includes(true)) {
       core.setFailed(`Expect at leat one review from the team: ${JSON.stringify(team)}`);
     }
   } catch (error) {
